ce42add2a55309e3106a5120815b167c63a75e9d
Ashis Kumar Sardar <ashissardar7003@gmail.com>
2023-01-12 21:37:10 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+
+    Run|debug
+    public static void main(string[] args){
+        int a = 4;
+        int b = 2;
+
+        system.out.println("The product is " + (a*b)); 
+}
+
+}
